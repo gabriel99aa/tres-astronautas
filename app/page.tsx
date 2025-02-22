47abe32c5374361d2dc000d696ccae3a1240bdc5
Gabriel Agudelo <gabrielestebanaa99@gmail.com>
2025-02-22 08:56:52 -0500

    animaciones

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,61 @@
 'use client';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { LayoutContext } from '@layout/context/layoutcontext';
+import { Tooltip } from 'primereact/tooltip';
 import type { Page } from '@/types';
 
 import './styles.css';
-import { Tooltip } from 'primereact/tooltip';
+
+const planets = [
+    { name: 'Mercurio', image: '/assets/mercurio.png', className: 'pd-planeta-left' },
+    { name: 'Venus', image: '/assets/venus.png', className: 'pd-planeta-right' },
+    { name: 'Tierra', image: '/assets/tierra.png', className: 'pd-planeta-left' },
+    { name: 'Marte', image: '/assets/marte.png', className: 'pd-planeta-right' },
+    { name: 'Júpiter', image: '/assets/jupiter.png', className: 'pd-planeta-left' },
+    { name: 'Saturno', image: '/assets/saturno.png', className: 'pd-planeta-right' },
+    { name: 'Urano', image: '/assets/urano.png', className: 'pd-planeta-left' },
+    { name: 'Neptuno', image: '/assets/neptuno.png', className: 'pd-planeta-right' }
+];
 
 const LandingPage: Page = () => {
     const { layoutConfig } = useContext(LayoutContext);
-    const router = useRouter();
     const path = '/layout/images/landing/';
     const image = layoutConfig.colorScheme === 'dark' ? 'line-effect-dark.svg' : 'line-effect.svg';
+    const [infinity, setInfinity] = useState(false);
 
-    const navigateToDashboard = () => {
-        router.push('/');
-    };
-    const navigateToRegister = () => {
-        router.push('/auth/login');
-    };
-
-    const scrollToElement = (el: React.RefObject<HTMLDivElement>) => {
-        setTimeout(() => {
-            el.current?.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
-                inline: 'nearest'
-            });
-        }, 200);
-    };
+    useEffect(() => {
+        setTimeout(() => setInfinity(true), 2000);
+    }, []);
 
     return (
-        <>
-            <div className="surface-ground min-h-screen">
-                <div className="landing-wrapper">
+        <div className="surface-ground min-h-screen">
+            <div className="landing-wrapper overflow-x">
+                <div
+                    style={{ backgroundImage: `url(${path}${image})`, paddingBottom: '280px' }}
+                    className="bg-no-repeat bg-cover bg-bottom min-h-screen"
+                >
                     <div
-                        style={{ backgroundImage: `url(${path}${image})` }}
-                        className="bg-no-repeat bg-cover bg-bottom min-h-screen"
-                    >
-                        <div
-                            style={{ backgroundImage: `url(/assets/sol.png)`, height: '32rem !important' }}
-                            className="bg-no-repeat bg-bottom animate__animated animate__fadeInDown"
-                        />
+                        style={{ backgroundImage: `url(/assets/sol.png)`, height: '32rem !important' }}
+                        className={`bg-no-repeat bg-bottom animate__animated ${
+                            infinity ? 'animate__pulse animate__infinite sun' : 'animate__fadeInDown'
+                        } `}
+                    />
 
-                        <Tooltip target=".sombra-levitacion" mouseTrack mouseTrackLeft={100} />
-                        <img
-                            src={'/assets/mercurio.png'}
-                            data-pr-tooltip="Mercurio"
-                            className="sombra-levitacion pd-planeta-left mercurio"
-                            alt="PLANETA"
-                        />
+                    <Tooltip target=".sombra-levitacion" mouseTrack mouseTrackLeft={100} />
 
+                    {planets.map(({ name, image, className }) => (
                         <img
-                            src={'/assets/venus.png'}
-                            data-pr-tooltip="Venus"
-                            className="sombra-levitacion pd-planeta-right venus"
-                            alt="PLANETA"
+                            key={name}
+                            src={image}
+                            data-pr-tooltip={name}
+                            className={`sombra-levitacion ${className}`}
+                            alt={name}
                         />
-                        <img
-                            src={'/assets/tierra.png'}
-                            data-pr-tooltip="Tierra"
-                            className="sombra-levitacion pd-planeta-left tierra"
-                            alt="PLANETA"
-                        />
-                        <img
-                            src={'/assets/marte.png'}
-                            data-pr-tooltip="Marte"
-                            className="sombra-levitacion pd-planeta-right marte"
-                            alt="PLANETA"
-                        />
-                        <img
-                            src={'/assets/jupiter.png'}
-                            data-pr-tooltip="Júpiter"
-                            className="sombra-levitacion pd-planeta-left jupiter"
-                            alt="PLANETA"
-                        />
-                        <img
-                            src={'/assets/saturno.png'}
-                            data-pr-tooltip="Saturno"
-                            className="sombra-levitacion pd-planeta-right saturno"
-                            alt="PLANETA"
-                        />
-                        <img
-                            src={'/assets/urano.png'}
-                            data-pr-tooltip="Urano"
-                            className="sombra-levitacion pd-planeta-left urano"
-                            alt="PLANETA"
-                        />
-                        <img
-                            src={'/assets/neptuno.png'}
-                            data-pr-tooltip="Neptuno"
-                            className="sombra-levitacion pd-planeta-right neptuno"
-                            alt="PLANETA"
-                        />
-                    </div>
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
