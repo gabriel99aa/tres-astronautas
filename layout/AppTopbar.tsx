78f0296f43a67b883f299a573bbfa7b1634c8145
Gabriel Agudelo <gabrielestebanaa99@gmail.com>
2025-02-24 11:14:32 -0500

    buscador

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -1,27 +1,47 @@
-import { InputText } from 'primereact/inputtext';
-import { forwardRef, useContext, useImperativeHandle, useRef, useState } from 'react';
-import { LayoutContext } from './context/layoutcontext';
-import type { AppTopbarRef } from '@/types';
+import { AutoComplete } from 'primereact/autocomplete';
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { classNames } from 'primereact/utils';
 
-const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
-    const { tabs, closeTab } = useContext(LayoutContext);
+interface Planet {
+    name: string;
+    link: string;
+}
 
+const planets: Planet[] = [
+    { name: 'Mercure', link: '/planet/mercure' },
+    { name: 'Venus', link: '/planet/venus' },
+    { name: 'Terre', link: '/planet/terre' },
+    { name: 'Mars', link: '/planet/mars' },
+    { name: 'Jupiter', link: '/planet/jupiter' },
+    { name: 'Saturne', link: '/planet/saturne' },
+    { name: 'Uranus', link: '/planet/uranus' },
+    { name: 'Neptune', link: '/planet/neptune' }
+];
+
+const AppTopbar = forwardRef((props, ref) => {
     const [searchActive, setSearchActive] = useState<boolean | null>(null);
-
-    const pathname = usePathname();
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [filteredResults, setFilteredResults] = useState<Planet[]>([]);
     const router = useRouter();
     const menubuttonRef = useRef(null);
 
+    const search = (event: { query: string }) => {
+        const query = event.query.toLowerCase();
+        const results = planets.filter((planet) => planet.name.toLowerCase().includes(query));
+        setFilteredResults(results);
+    };
+
+    const onSelect = (e: { value: Planet }) => {
+        router.push(e.value.link);
+        setSearchActive(false);
+    };
+
     const activateSearch = () => {
         setSearchActive(true);
-        setTimeout(() => {
-            const element = document.querySelector('.searchInput');
-            (element as HTMLElement)?.focus();
-        }, 100);
     };
+
     const deactivateSearch = () => {
         setSearchActive(false);
     };
@@ -30,51 +50,38 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
         menubutton: menubuttonRef.current
     }));
 
-    const onCloseTab = (index: number) => {
-        if (tabs.length > 1) {
-            if (index === tabs?.length - 1) router.push(tabs?.[tabs.length - 2].to);
-            else router.push(tabs?.[index + 1].to);
-        } else {
-            router.push('/');
-        }
-        closeTab(index);
-    };
-
     return (
         <div className="layout-topbar">
             <Link href={'/'} className="app-logo">
                 <img alt="app logo" src="/assets/sol.png" />
                 <span className="app-name">Solar System</span>
             </Link>
-
-            <ul className="topbar-menu">
-                {tabs.map((item, i) => {
-                    return (
-                        <li key={i}>
-                            <Link href={item.to} className={classNames({ 'active-route': item.to === pathname })}>
-                                <span>{item.label}</span>
-                            </Link>
-                            <i className="pi pi-times" onClick={() => onCloseTab(i)}></i>
-                        </li>
-                    );
-                })}
-                {!tabs || (tabs.length === 0 && <li className="topbar-menu-empty"></li>)}
-            </ul>
-
-            <div
-                className={classNames('topbar-search', {
-                    'topbar-search-active': searchActive
-                })}
-            >
-                <button className="topbar-searchbutton p-link" onClick={activateSearch}>
-                    <i className="pi pi-search"></i>
-                </button>
-
-                <div className="search-input-wrapper">
-                    <span className="p-input-icon-right">
-                        <InputText className="searchInput" type="text" placeholder="Search" onBlur={deactivateSearch} />
+            <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
+                <div
+                    className={classNames('topbar-search', {
+                        'topbar-search-active': searchActive
+                    })}
+                >
+                    <button className="topbar-searchbutton p-link" onClick={activateSearch}>
                         <i className="pi pi-search"></i>
-                    </span>
+                    </button>
+
+                    <div className="search-input-wrapper">
+                        <span className="p-input-icon-right">
+                            <AutoComplete
+                                className="searchInput"
+                                value={searchTerm}
+                                suggestions={filteredResults}
+                                completeMethod={search}
+                                field="name"
+                                placeholder="Search a planet"
+                                onChange={(e) => setSearchTerm(e.value)}
+                                onSelect={onSelect}
+                                onBlur={deactivateSearch}
+                            />
+                            <i className="pi pi-search"></i>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
